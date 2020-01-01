19a90ededee5648999cab5309fc01e231a598131
djk18 <djk18@imperial.ac.uk>
2020-01-01 16:19:48 +0000

    Implemented size and isEmpty method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,29 @@
-public class LinkedList {
+public class LinkedList<E> {
+  private final Node head, tail;
+  private int size;
+
+  LinkedList() {
+    this.head = new Node(null);
+    this.tail = new Node(null);
+    this.size = 0;
+  }
+
+  public boolean isEmpty() {
+    return this.size() == 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  private class Node<E> {
+    Node next, prev;
+    E data;
+
+    Node(E data){
+      this.data = data;
+      this.prev = null;
+      this.next = null;
+    }
+  }
 }
--- test/LinkedListTest.java
+++ test/LinkedListTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LinkedListTest {
 
+  @Test
+  public void isEmptyOnInitialisation() {
+    assertTrue(new LinkedList<String>().isEmpty());
+  }
 }
